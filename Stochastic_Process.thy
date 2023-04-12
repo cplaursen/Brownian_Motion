0282c37903dacecb1c219672076b4d4520e6dc54
Christian Pardillo Laursen <christiansfd@protonmail.com>
2023-04-12 17:27:36 +0100

    Finish proving modification_continuous_indistinguishable

--- Stochastic_Process.thy
+++ Stochastic_Process.thy
@@ -1,26 +1,31 @@
 theory Stochastic_Process
-  imports "HOL-Probability.Probability"
+  imports "HOL-Probability.Probability" "Dyadic_Interval"
 begin
 
 text \<open> A stochastic process is an indexed collection of random variables. For compatibility with 
   product_prob_space we don't mention the index set I in the assumptions. \<close>
 
+text_raw \<open>\DefineSnippet{stochastic_process}{\<close>
 locale stochastic_process = prob_space +
   fixes M' :: "'b measure"
     and I :: "'t set"
     and X :: "'t \<Rightarrow> 'a \<Rightarrow> 'b"
-  assumes random_X[measurable]: "\<And>i. random_variable M' (X i)"
-                                                  
-sublocale stochastic_process \<subseteq> prod: product_prob_space "(\<lambda>t. distr M M' (X t))"
-  using prob_space_distr random_X by (blast intro: product_prob_spaceI)
+  assumes random_process[measurable]: "\<And>i. random_variable M' (X i)"
+text_raw \<open>}%EndSnippet\<close>
+
+sublocale stochastic_process \<subseteq> product: product_prob_space "(\<lambda>t. distr M M' (X t))"
+  using prob_space_distr random_process by (blast intro: product_prob_spaceI)
 
 lemma (in prob_space) stochastic_processI:
   assumes "\<And>i. random_variable M' (X i)"
     shows "stochastic_process M M' X"
   by (simp add: assms prob_space_axioms stochastic_process_axioms.intro stochastic_process_def)
 
-typedef ('t, 'a, 'b) stochastic_process = "{(M :: 'a measure, M' :: 'b measure, I :: 't set, X :: 't \<Rightarrow> 'a \<Rightarrow> 'b).
+text_raw \<open>\DefineSnippet{stochastic_process_typedef}{\<close>
+typedef ('t, 'a, 'b) stochastic_process =
+ "{(M :: 'a measure, M' :: 'b measure, I :: 't set, X :: 't \<Rightarrow> 'a \<Rightarrow> 'b).
    stochastic_process M M' X}"
+text_raw \<open>}%EndSnippet\<close>
 proof
   show "(return (sigma UNIV {{}, UNIV}) x, sigma UNIV UNIV, UNIV, \<lambda>_ _. c) \<in>
        {(M, M', I, X). stochastic_process M M' X}" for x :: 'a and c :: 'b
@@ -47,19 +52,10 @@ is "snd \<circ> snd \<circ> snd" .
 declare [[coercion process]]
 
 lemma stochastic_process_construct [simp]: "stochastic_process (proc_source X) (proc_target X) (process X)"
-  apply induction unfolding proc_source_def proc_target_def process_def by (auto simp: Abs_stochastic_process_inverse)
+  by (transfer, force)
 
 interpretation stochastic_process "proc_source X" "proc_target X" "proc_index X" "process X"
-  by (fact stochastic_process_construct)
-
-lemma
-  assumes "stochastic_process M M' X"
-  shows Abs_process_source[simp]: "proc_source (Abs_stochastic_process (M, M', I, X)) = M" and
-        Abs_process_target[simp]: "proc_target (Abs_stochastic_process (M, M', I, X)) = M'" and
-        Abs_process_index[simp]: "proc_index (Abs_stochastic_process (M, M', I, X)) = I" and
-        Abs_process_process[simp]: "process (Abs_stochastic_process (M, M', I, X)) = X"
-  unfolding proc_source_def proc_target_def proc_index_def process_def
-  using assms by (simp_all add: Abs_stochastic_process_inverse)
+  by simp
 
 text \<open> Here we construct a process on a given index set. For this we need to produce measurable
   functions for indices outside the index set; we use the constant function, but it needs to point at
@@ -89,23 +85,28 @@ lemma process_of_apply:
 
 text \<open> We define the finite-dimensional distributions of our process. \<close>
 
-definition distributions :: "('t, 'a, 'b) stochastic_process \<Rightarrow> 't set \<Rightarrow> ('t \<Rightarrow> 'b) measure" where
-"distributions X T = (\<Pi>\<^sub>M t\<in>T. distr (proc_source X) (proc_target X) (X t))"
+text_raw \<open>\DefineSnippet{distributions}{\<close>
+lift_definition distributions :: "('t, 'a, 'b) stochastic_process \<Rightarrow> 't set \<Rightarrow> ('t \<Rightarrow> 'b) measure"
+is "\<lambda>(M, M', _, X) T. (\<Pi>\<^sub>M t\<in>T. distr M M' (X t))"
+text_raw \<open>}%EndSnippet\<close> .
+
+lemma distributions_altdef: "distributions X T = (\<Pi>\<^sub>M t\<in>T. distr (proc_source X) (proc_target X) (X t))"
+  by (transfer, auto)
 
 lemma prob_space_distributions: "prob_space (distributions X J)"
-  unfolding distributions_def
-  by (simp add: prob_space_PiM proc_source.prob_space_distr stochastic_process.random_X)
+  unfolding distributions_altdef
+  by (simp add: prob_space_PiM proc_source.prob_space_distr random_process)
 
 lemma sets_distributions: "sets (distributions X J) = sets (PiM J (\<lambda>_. (proc_target X)))"
-  unfolding distributions_def by (rule sets_PiM_cong; simp)
+  by (transfer, auto cong: sets_PiM_cong)
 
 lemma space_distributions: "space (distributions X J) = (\<Pi>\<^sub>E i\<in>J. space (proc_target X))"
-  unfolding distributions_def by (simp add: space_PiM)
+  by (transfer, auto simp add: space_PiM)
 
 lemma emeasure_distributions:
   assumes "finite J" "\<And>j. j\<in>J \<Longrightarrow> A j \<in> sets (proc_target X)"
   shows "emeasure (distributions X J) (Pi\<^sub>E J A) = (\<Prod>j\<in>J. emeasure (distr (proc_source X) (proc_target X) (X j)) (A j))"
-  by (simp add: distributions_def assms prod.emeasure_PiM)
+  by (simp add: assms(1) assms(2) distributions_altdef product.emeasure_PiM)
 
 interpretation projective_family "(proc_index X)" "distributions X" "(\<lambda>_. proc_target X)"
 proof (intro projective_family.intro)
@@ -130,7 +131,7 @@ proof (intro projective_family.intro)
       apply (rule emeasure_distr_restrict)
       by (simp_all add: "*" sets_distributions in_sets)
     also have "... = emeasure (distributions X J) S"
-      unfolding distributions_def
+      unfolding distributions_altdef
         using *(1,2) in_sets prod_emb_distr by force
     finally show "emeasure (distributions X J) S 
                 = emeasure (distr (distributions X H) (Pi\<^sub>M J (\<lambda>_. ?M')) (\<lambda>f. restrict f J)) S"
@@ -182,9 +183,12 @@ lemma (in prob_space) indep_vars_indep_var:
 
 text \<open> For all sorted lists of indices, the increments specified by this list are independent \<close>
 
+
+text_raw \<open>\DefineSnippet{indep_increments}{\<close>
 definition (in prob_space) "indep_increments M' X I \<longleftrightarrow>
   (\<forall>l. set l \<subseteq> I \<and> sorted l \<and> length l \<ge> 2 \<longrightarrow>
      indep_vars (\<lambda>_. M') (\<lambda>k v. X (l!k) v - X (l!(k-1)) v) {1..<length l})"
+text_raw \<open>}%EndSnippet\<close>
 
 lemma (in prob_space) indep_incrementsE:
   assumes "indep_increments M' X I"
@@ -217,15 +221,20 @@ proof -
     by simp
 qed
 
+text_raw \<open>\DefineSnippet{stationary_increments}{\<close>
 definition (in prob_space) "stationary_increments M' X \<longleftrightarrow>
 (\<forall>t1 t2 k. t1 > 0 \<and> t2 > 0 \<and> k > 0 \<longrightarrow> 
      distr M M' (\<lambda>v. X (t1 + k) v - X t1 v) = distr M M' (\<lambda>v. X (t2 + k) v - X t2 v))"
+text_raw \<open>}%EndSnippet\<close>
 
 text \<open> Processes on the same source measure space, with the same index space, but not necessarily the same
   target measure since we only care about the measurable target space, not the measure \<close>
+
+text_raw \<open>\DefineSnippet{compatible}{\<close>
 definition compatible :: "('t,'a,'b) stochastic_process \<Rightarrow> ('t,'a,'b) stochastic_process \<Rightarrow> bool" where
 "compatible X Y \<longleftrightarrow> proc_source X = proc_source Y \<and> sets (proc_target X) = sets (proc_target Y)
   \<and> proc_index X = proc_index Y"
+text_raw \<open>}%EndSnippet\<close>
 
 lemma compatibleI:
   assumes "proc_source X = proc_source Y" "sets (proc_target X) = sets (proc_target Y)"
@@ -258,8 +267,10 @@ lemma compatible_process_of:
   shows "compatible (process_of M M' I X a) (process_of M M' I Y b)"
   unfolding compatible_def using assms by force
 
+text_raw \<open>\DefineSnippet{modification}{\<close>
 definition modification :: "('t,'a,'b) stochastic_process \<Rightarrow> ('t,'a,'b) stochastic_process \<Rightarrow> bool" where
 "modification X Y \<longleftrightarrow> compatible X Y \<and> (\<forall>t \<in> proc_index X. AE x in proc_source X. X t x = Y t x)"
+text_raw \<open>}%EndSnippet\<close>
 
 lemma modificationI:
   assumes "compatible X Y" "(\<forall>t \<in> proc_index X. AE x in proc_source X. X t x = Y t x)"
@@ -318,9 +329,11 @@ proof (intro modificationI, safe)
     using XY by fastforce
 qed
 
+text_raw \<open>\DefineSnippet{indistinguishable}{\<close>
 definition indistinguishable :: "('t,'a,'b) stochastic_process \<Rightarrow> ('t,'a,'b) stochastic_process \<Rightarrow> bool" where
 "indistinguishable X Y \<longleftrightarrow> compatible X Y \<and> (\<exists>N \<in> null_sets (proc_source X).
   \<forall>t \<in> proc_index X. {x \<in> space (proc_source X). X t x \<noteq> Y t x} \<subseteq> N)"
+text_raw \<open>}%EndSnippet\<close>
 
 lemma indistinguishableI:
   assumes "compatible X Y" 
@@ -385,9 +398,11 @@ qed
 
 text \<open> Klenke 21.5(i) \<close>
 
+text_raw \<open>\DefineSnippet{modification_countable}{\<close>
 lemma modification_countable:
   assumes "modification X Y" "countable (proc_index X)"
   shows "indistinguishable X Y"
+text_raw \<open>}%EndSnippet\<close>
 proof (rule indistinguishableI)
   show "compatible X Y"
     using assms(1) modification_def by auto
@@ -412,6 +427,7 @@ qed
 
 text \<open> Klenke 21.5(ii). His statement is more general - we reduce right continuity to regular continuity \<close>
 
+text_raw \<open>\DefineSnippet{modification_continuous_indistinguishable}{\<close>
 lemma modification_continuous_indistinguishable:
   fixes X :: "(real, 'a, 'b :: metric_space) stochastic_process"
   assumes modification: "modification X Y"
@@ -421,69 +437,85 @@ lemma modification_continuous_indistinguishable:
               "AE \<omega> in proc_source Y. continuous_on (proc_index Y) (\<lambda>t. Y t \<omega>)"
                   (is "AE \<omega> in proc_source Y. ?cont_Y \<omega>")
   shows "indistinguishable X Y"
+text_raw \<open>}%EndSnippet\<close>
 proof (rule indistinguishableI)
   show "compatible X Y"
     using modification modification_def by blast
   obtain T where T: "proc_index X = {0..T}" "T > 0"
     using interval by blast
-  let ?N = "\<lambda>t. {x \<in> space (proc_source X). X t x \<noteq> Y t x}"
-  have 1: "\<forall>t \<in> proc_index X. \<exists>S. ?N t \<subseteq> S \<and> S \<in> null_sets (proc_source X)"
-    using modificationD(2)[OF modification] by (auto simp add: eventually_ae_filter)
-  obtain S where S: "\<forall>t \<in> proc_index X. ?N t \<subseteq> S t
-   \<and> S t \<in> null_sets (proc_source X)"
+  define N where "N \<equiv> \<lambda>t. {x \<in> space (proc_source X). X t x \<noteq> Y t x}"
+  have 1: "\<forall>t \<in> proc_index X. \<exists>S. N t \<subseteq> S \<and> S \<in> null_sets (proc_source X)"
+    using modificationD(2)[OF modification] by (auto simp add: N_def eventually_ae_filter)
+  text \<open> S is a null set such that X t x \<noteq> Y t x ==> x \<in> S t \<close>
+  obtain S where S: "\<forall>t \<in> proc_index X. N t \<subseteq> S t \<and> S t \<in> null_sets (proc_source X)"
     using bchoice[OF 1] by blast
   have eq: "proc_source X = proc_source Y" "proc_index X = proc_index Y"
     using \<open>compatible X Y\<close> compatible_source compatible_index by blast+
   have "AE p in proc_source X. ?cont_X p \<and> ?cont_Y p"
     apply (rule AE_conjI)
     using eq rc by argo+
+  text \<open> R is a set of measure 1 such that if x \<in> R then the paths at x are continuous for X and Y \<close>
   then obtain R where R: "R \<subseteq> {p \<in> space (proc_source X). ?cont_X p \<and> ?cont_Y p}"
     "R \<in> sets (proc_source X)" "measure (proc_source X) R = 1"
     using proc_source.AE_E_prob by blast
-  let ?I = "\<rat> \<inter> {0..T}"
-  have "countable ?I"
-    using countable_rat by blast
-  {
+  text \<open> We use an interval of dyadic rationals because we need to produce a countable dense set
+  for {0..T}, which we have by @{thm dyadic_interval_dense}. \<close>
+  let ?I = "dyadic_interval 0 T"
+  let ?N' = "\<Union>n \<in> ?I. N n"
+  have N_subset: "\<And>t. t \<in> proc_index X \<Longrightarrow> N t \<inter> R \<subseteq> ?N'"
+  proof
     fix t assume "t \<in> proc_index X"
-    then have "?N t \<inter> R \<subseteq> (\<Union>r \<in> ?I. ?N r \<inter> R)"
-    proof (auto simp add: subset_eq)
-      fix p
-      assume *: "t \<in> proc_index X" "p \<in> R" "X t p \<noteq> Y t p"
-      have continuous_p: "continuous_on {0..T} (\<lambda>t. Y t p)"
-           "continuous_on {0..T} (\<lambda>t. X t p)"
-         using R *(2) T(1)[symmetric] eq(2) by auto        
-      then obtain \<epsilon> where e: "\<epsilon> > 0 \<and> \<epsilon> = dist (X t p) (Y t p)"
-        using *(3) by auto
-      {
-        assume "\<not> (\<exists>r\<in> ?I. X r p \<noteq> Y r p)"
-        then have "\<forall>r \<in> ?I. X r p = Y r p"
-          by presburger
-        then have "\<exists>d \<in> ?I. d < \<epsilon> \<and> dist (X t p) (Y t p) < d"
-          using continuous_p sorry (* follows from continuity and connectedness *)
-        then have False
-          using e by force
-      }
-      then show "(\<exists>xa\<in> ?I. process X xa p \<noteq> process Y xa p)"
-        by blast
-    qed
-    also have "... \<subseteq> (\<Union>r \<in> ?I. ?N r)"
-      by blast
-    finally have "?N t \<inter> R \<subseteq> (\<Union>r \<in> ?I. ?N r)" .
-  } note N_tilde = this
+    fix p assume *: "p \<in> N t \<inter> R"
+    then obtain \<epsilon> where \<epsilon>: "0 < \<epsilon>" "\<epsilon> = dist (X t p) (Y t p)"
+      by (simp add: N_def)
+    have cont_p: "continuous_on {0..T} (\<lambda>t. Y t p)" "continuous_on {0..T} (\<lambda>t. X t p)"
+      using R *(1) T(1)[symmetric] eq(2) by auto
+    then have continuous_dist: "continuous_on {0..T} (\<lambda>t. dist (X t p) (Y t p))"
+      using continuous_on_dist by fast
+    {
+      assume "\<forall>r\<in> ?I. X r p = Y r p"
+      then have dist_0: "\<And>r. r \<in> ?I \<Longrightarrow> dist (X r p) (Y r p) = 0"
+        by auto
+      have "dist (X t p) (Y t p) = 0"
+      proof -
+        have dist_tendsto_0: "((\<lambda>t. dist (X t p) (Y t p)) \<longlongrightarrow> 0)(at t within ?I)"
+          using dist_0 continuous_dist
+          by (smt (verit, best) Lim_transform_within \<epsilon> tendsto_const)
+        have XY: "((\<lambda>t. X t p) \<longlongrightarrow> X t p)(at t within ?I)" "((\<lambda>t. Y t p) \<longlongrightarrow> Y t p)(at t within ?I)"
+          by (metis cont_p T(1) \<open>t \<in> proc_index X\<close> continuous_on_def tendsto_within_subset dyadic_interval_subset_interval)+
+        show ?thesis
+          apply (rule tendsto_unique[of "at t within ?I"])
+          defer
+          using tendsto_dist[OF XY] dist_tendsto_0 apply simp
+          using tendsto_dist[OF XY] dist_tendsto_0 apply simp
+          apply (simp add: trivial_limit_within)
+          by (metis T(1) T(2) \<open>t \<in> proc_index X\<close> dyadic_interval_dense islimpt_Icc limpt_of_closure)
+      qed
+      then have False
+        using \<epsilon> by force
+    }
+    then have "\<exists>r\<in>dyadic_interval 0 T. p \<in> N r"
+      unfolding N_def using * R(2) sets.sets_into_space by auto
+    then show "p \<in> \<Union> (N ` ?I)"
+      by simp
+  qed
+  thm N_subset
   have null: "(space (proc_source X) - R) \<union> (\<Union>r \<in> ?I. S r) \<in> null_sets (proc_source X)"
     apply (rule null_sets.Un)
      apply (smt (verit) R(2,3) AE_iff_null_sets proc_source.prob_compl proc_source.prob_eq_0 sets.Diff sets.top)
-      using \<open>countable ?I\<close> null_sets_UN' S T(1) by blast
-  have "(\<Union>r \<in> proc_index X. S r) \<subseteq> (space (proc_source X) - R) \<union> (\<Union>r \<in> proc_index X. S r)"
+    by (metis (no_types, lifting) S T(1) dyadic_interval_countable dyadic_interval_subset_interval in_mono null_sets_UN')
+  have "(\<Union>r \<in> proc_index X. N r) \<subseteq> (space (proc_source X) - R) \<union> (\<Union>r \<in> proc_index X. N r)"
     by blast
+  also have "... \<subseteq> (space (proc_source X) - R) \<union> (\<Union>r \<in> ?I. N r)"
+    using N_subset N_def by blast
   also have "... \<subseteq> (space (proc_source X) - R) \<union> (\<Union>r \<in> ?I. S r)"
-    using N_tilde S sorry
+    by (smt (verit, ccfv_threshold)S T(1) UN_iff Un_iff dyadic_interval_subset_interval in_mono subsetI)
   finally show "\<exists>N\<in>null_sets (proc_source X). \<forall>t\<in>proc_index X. {x \<in> space (proc_source X). process X t x \<noteq> process Y t x} \<subseteq> N"
-    by (smt (verit) null S SUP_le_iff order_trans)
+    by (smt (verit) N_def null S SUP_le_iff order_trans)
 qed
 
-definition restrict_index :: "('t, 'a, 'b) stochastic_process \<Rightarrow> 't set \<Rightarrow> ('t, 'a, 'b) stochastic_process"
-  where "restrict_index X T \<equiv> Abs_stochastic_process ((proc_source X), (proc_target X), T, (process X))"
+lift_definition restrict_index :: "('t, 'a, 'b) stochastic_process \<Rightarrow> 't set \<Rightarrow> ('t, 'a, 'b) stochastic_process"
+ is "\<lambda>(M, M', I, X) T. (M, M', T, X)" by fast
 
 lemma
   shows
@@ -491,10 +523,10 @@ lemma
   restrict_index_target[simp]: "proc_target (restrict_index X T) = proc_target X" and
   restrict_index_index[simp]:  "proc_index (restrict_index X T) = T" and
   restrict_index_process[simp]: "process (restrict_index X T) = process X"
-  unfolding restrict_index_def by simp_all
+  by (transfer, force)+
 
 lemma restrict_index_override[simp]: "restrict_index (restrict_index X T) S = restrict_index X S"
-  unfolding restrict_index_def by simp
+  by (transfer, auto)
 
 lemma compatible_restrict_index:
   assumes "compatible X Y"
@@ -517,6 +549,15 @@ lemma indistinguishable_restrict_index:
 lemma modification_imp_identical_distributions:
   assumes "modification X Y" "T \<subseteq> proc_index X"
   shows "distributions X T = distributions Y T"
-  oops
+  unfolding distributions_altdef
+  apply (rule PiM_cong)
+   apply simp
+  apply (rule distr_cong_AE)
+  using assms(1) compatible_source modificationD(1) apply auto[1]
+  using assms(1) compatible_target modificationD(1) apply blast
+    apply (metis assms modificationD(2) subset_eq)
+   apply simp
+  using assms(1) modificationD(1) apply simp
+  done
 
 end
